0fbad4ab3a1941bcb00084d5d91882db29b5188d
Makram Kamaleddine <makram.kd@gmail.com>
2016-01-21 15:42:53 -0500

    Adding a trivial test case as a sanity check

--- main.cpp
+++ main.cpp
@@ -1,8 +1,21 @@
 #include <iostream>
 
-using namespace std;
+#include "matrix.hpp"
 
 int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+    nvector<double> nvector1(3), nvector2(3);
+    for (int i = 0; i < nvector1.size(); ++i) {
+        nvector1[i] = i;
+    }
+
+    for (int i = 0; i < nvector2.size(); ++i) {
+        nvector2[i] = i * i + 1;
+    }
+
+    std::cout << "Vector 1:" << std::endl;
+    std::cout << nvector1 << std::endl;
+    std::cout << "Vector 2:" << std::endl;
+    std::cout << nvector2 << std::endl;
+
+    std::cout << "The inner product (standard) : " << inner_product(nvector1, nvector2) << std::endl;
 }
