7828ec0314cda6bd093d2d44b531cac8ae14fa2e
Makram Kamaleddine <makram.kd@gmail.com>
2016-01-21 20:40:17 -0500

    Taking the alias template out into a common header file

---
+++ common.hpp
@@ -0,0 +1,14 @@
+//
+// Created by Makram Kamaleddine on 1/21/16.
+//
+
+#ifndef QR_DECOMPOSITION_COMMON_HPP
+#define QR_DECOMPOSITION_COMMON_HPP
+
+#include <vector>
+
+// alias template for a container type
+template<typename Type>
+using container = std::vector<Type>;
+
+#endif //QR_DECOMPOSITION_COMMON_HPP
