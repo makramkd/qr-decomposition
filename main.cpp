7488cccec39aec69cd7e9739007220c6e7ee124a
Makram Kamaleddine <makram.kd@gmail.com>
2016-01-24 20:37:42 -0500

    Add testing code

--- main.cpp
+++ main.cpp
@@ -4,12 +4,22 @@
 #include "ops.hpp"
 
 int main() {
-    container<container<float>> basis;
-    basis.push_back({1.5, 2.5, 3.5});
-    basis.push_back({3.5, 4.5, 5.5});
-    basis.push_back({3.5, 8.2, 1.2});
+    using std::cout;
 
-    auto mat = construct_from_column_vectors(basis);
+    container<nvector<float>> basis;
+    basis.push_back(nvector<float>(3, {12, 6, -4}));
+    basis.push_back(nvector<float>(3, {-51, 167, 24}));
+    basis.push_back(nvector<float>(3, {4, -68, -41}));
 
-    std::cout << "Matrix: \n" << mat << "\n";
+    cout << "Basis before orthonormalization: \n";
+    for (auto i = basis.begin(); i != basis.end(); ++i) {
+        cout << *i << "\n";
+    }
+
+    auto orthonormal = orthonormalize(basis);
+
+    cout << "Orthonormalized Basis: \n";
+    for (auto i = orthonormal.begin(); i != orthonormal.end(); ++i) {
+        cout << *i << "\n";
+    }
 }
