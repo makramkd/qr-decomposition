8e1847da51a02cf538cfe6bf73557dbc40bc64f1
Makram Kamaleddine <makram.kd@gmail.com>
2016-01-24 21:09:49 -0500

    update test code

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 int main() {
     using std::cout;
 
-    container<nvector<float>> basis;
-    basis.push_back(nvector<float>(3, {12, 6, -4}));
-    basis.push_back(nvector<float>(3, {-51, 167, 24}));
-    basis.push_back(nvector<float>(3, {4, -68, -41}));
+    container<nvector<double>> basis;
+    basis.push_back(nvector<double>(3, {12, 6, -4}));
+    basis.push_back(nvector<double>(3, {-51, 167, 24}));
+    basis.push_back(nvector<double>(3, {4, -68, -41}));
 
     cout << "Basis before orthonormalization: \n";
     for (auto i = basis.begin(); i != basis.end(); ++i) {
@@ -17,12 +17,16 @@ int main() {
         cout << i->norm() << "\n";
     }
 
-    auto orthonormal = orthonormalize(basis, [](float v1, float v2) {
+    auto std_inner = [](double v1, double v2) {
         return v1 * v2;
-    });
+    };
+    auto orthonormal = orthonormalize(basis, std_inner);
 
     cout << "Orthonormalized Basis: \n";
     for (auto i = orthonormal.begin(); i != orthonormal.end(); ++i) {
         cout << *i << "\n";
     }
+
+    auto check = check_orthonormality(orthonormal, std_inner);
+    cout << "Orthonormal sum: " << check << "\n";
 }
